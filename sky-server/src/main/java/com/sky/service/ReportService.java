bd863340f8239cdbd3f886e098f3ba1521d03ea1
laputae <ahahahay51@gmail.com>
2026-02-22 17:36:12 +0800

    导出Excel报表接口

--- sky-server/src/main/java/com/sky/service/ReportService.java
+++ sky-server/src/main/java/com/sky/service/ReportService.java
@@ -5,6 +5,7 @@ import com.sky.vo.SalesTop10ReportVO;
 import com.sky.vo.TurnoverReportVO;
 import com.sky.vo.UserReportVO;
 
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
 
 public interface ReportService {
@@ -42,4 +43,11 @@ public interface ReportService {
      * @param end
      */
     SalesTop10ReportVO getTop10(LocalDate begin, LocalDate end);
+
+    /**
+     * 导出近30天的运营数据报表
+     *
+     * @param response
+     **/
+    void exportBusinessData(HttpServletResponse response);
 }
